bba2918d52b4e1965ad919782553357f8e1fc151
Narindra Balkissoon <narindrab4@gmail.com>
2021-05-22 14:32:31 -0400

    added textfields to signup card

--- src/pages/SIgnUp.js
+++ src/pages/SIgnUp.js
@@ -6,6 +6,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { ReactComponent as Logo } from '../assets/devchallenges.svg';
 const useStyles = makeStyles({
@@ -20,11 +21,29 @@ const useStyles = makeStyles({
         transform: 'translate(-50%, -50%)'
     },
     contentMargin: {
-        margin: 58
+       margin: "3rem 3.625rem"
     },
-    text: {
-        font: "'Noto Sans', sans-serif"
-    }
+    text1: {
+        fontFamily: ['Noto Sans', 'sans-serif'],
+        fontWeight: 600,
+        fontSize: "18px",
+        margin: '2.125rem 0 0.825rem'
+    },
+    text2: {
+      fontFamily: ['Noto Sans', 'sans-serif'],
+      fontSize: '0.825rem'
+    },
+    textFields: {
+      width: '100%'
+    },
+    button: {
+      fontFamily: ['Noto Sans', 'sans-serif'],
+      width: '100%',
+      background: '#2F80ED',
+      borderRadius: '8px',
+      color: '#fff',
+      textTransform: 'none'
+    },
   });
   
   export default function SignUp() {
@@ -34,12 +53,30 @@ const useStyles = makeStyles({
       <Card className={classes.root}>
           <CardContent className={classes.contentMargin}>
               <Logo className="logo" />
-            <Typography className={classes.text} gutterBottom variant="h6" component="p">
+            <Typography className={classes.text1} gutterBottom variant="h5" component="p">
             Join thousands of learners from around the world 
             </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
+            <Typography color="textSecondary" className={classes.text2} variant="body2" component="p">
             Master web development by making real-life projects. There are multiple paths for you to choose
             </Typography>
+            <TextField
+              id="outlined-secondary"
+              label="Email"
+              variant="outlined"
+              color="secondary"
+              className={classes.textFields}
+          />
+          <TextField
+              id="outlined-secondary"
+              label="Password"
+              variant="outlined"
+              color="secondary"
+              className={classes.textFields}
+          />
+          <Button className={classes.button}>Start coding now</Button>
+          <Typography className={classes.text2} style={{textAlign: 'center', color: '#828282', fontSize: '0.825rem'}} variant="body2" component="p">
+            or continue with these social profile
+          </Typography>
           </CardContent>
       </Card>
     );
